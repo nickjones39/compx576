e45a6bc60541dd66c0be99f73cd52961471e367d
nickjones39 <tabulanick@gmail.com>
2021-07-24 12:44:03 +1200

    1 file modified

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
  console.log(`Server is listening on port: ${PORT}`);
 });
