efd410611f62fc888468106bc0419638463b1709
Nick <nj78@students.waikato.ac.nz>
2021-07-31 10:52:23 +1200

    Express view engine, handlebars installed.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
@@ -6,26 +7,32 @@ const userRouter = require('./routers/user')
 const assetRouter = require('./routers/asset')
 
 const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 3000
 
+app.set('view engine', 'hbs')
 // app.use((req, res, next) => {
 //     res.status(503).send('Site currently down. Check back soon!')
 // })
 
+app.get('', (req, res) => {
+    res.render('index')
+})
 
 app.use(express.json())
 app.use(userRouter)
 app.use(assetRouter)
 
+app.use(express.static(publicDirectoryPath))
 
 app.listen(port, () => {
     console.log('Server is up on: ' + port)
 })
 
-const main = async () => {
-    const user = await User.findById('xxx')
-    await user.populate('assets').execPopulate()
-    console.log(user.assets)
-}
+// const main = async () => {
+//     const user = await User.findById('xxx')
+//     await user.populate('assets').execPopulate()
+//     console.log(user.assets)
+// }
 
-main()
+// main()
