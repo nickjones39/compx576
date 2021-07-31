c36a01ffd3170525dc4377f11c38ccdefc0a1a69
Nick <nj78@students.waikato.ac.nz>
2021-07-31 18:00:33 +1200

    Working on frontend

--- src/routers/user.js
+++ src/routers/user.js
@@ -56,21 +56,21 @@ router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
 
-// router.get('/users/:id', async (req, res) => {
-//     const _id = req.params.id
+router.get('/users/:id', async (req, res) => {
+    const _id = req.params.id
 
-//     try {
-//         const user = await User.findById(_id)
+    try {
+        const user = await User.findById(_id)
 
-//         if (!user) {
-//             return res.status(404).send()
-//         }
+        if (!user) {
+            return res.status(404).send()
+        }
 
-//         res.send(user)
-//     } catch (e) {
-//         res.status(500).send()
-//     }
-// })
+        res.send(user)
+    } catch (e) {
+        res.status(500).send()
+    }
+})
 
 
 // router.patch('/users/:id', async (req, res) => {
