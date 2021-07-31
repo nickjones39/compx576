f39b389cbab1b24ed5f8cb15dcf1073c712cb725
Nick <nj78@students.waikato.ac.nz>
2021-07-31 17:55:00 +1200

    Working on front-end

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,17 +3,29 @@ const User = require('../models/user')
 const auth = require('../middleware/auth')
 const router = new express.Router()
 
-router.post('/users', async (req, res) => {
+
+router.post('/users', (req, res) => {
     const user = new User(req.body)
 
-    try {
-        await user.save()
-        const token = await user.generateAuthToken()
-        res.status(201).send({ user, token })
-    } catch (e) {
+    user.save().then(() => {
+        res.status(201).send(user)
+    }).catch((e) => {
         res.status(400).send(e)
-    }
+    })
 })
+
+
+// router.post('/users', async (req, res) => {
+//     const user = new User(req.body)
+
+//     try {
+//         await user.save()
+//         const token = await user.generateAuthToken()
+//         res.status(201).send({ user, token })
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
 
 router.post('/users/login', async (req, res) => {
     try {
