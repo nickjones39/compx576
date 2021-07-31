05087333a2cf763001b0bf142c23c1f8ae595c5d
Nick <nj78@students.waikato.ac.nz>
2021-07-31 18:28:18 +1200

    Working on front-end

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/user')
 
 const auth = async (req, res, next) => {
     try {
-        const token = req.header('Authorisation').replace('Bearer ', '')
+        const token = req.header('Authorization').replace('Bearer ', '')
         const decoded = jwt.verify(token, 'SEED4compx576')
         const user = await User.findOne({ _id: decoded._id, 'tokens.token': token })
 
