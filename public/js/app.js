b053b2c7fb9e2ac0f9c81b3c3a829f0b267b154b
Nick <nj78@students.waikato.ac.nz>
2021-07-31 19:54:12 +1200

    bug fixed in app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,37 +1,17 @@
 
-
-const credentials = new Array ()
-
 const loginForm = document.getElementById('loginForm')
-
-credentials[0] = document.getElementById('email')
-credentials[1] = document.getElementById('password')
 
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
-    const email = credentials[0].value
-    const password = credentials[1].value
-
-    postData('https://compx576.herokuapp.com/users/login', { email: email, password: password })
+    postData('https://compx576.herokuapp.com/users/login', { email: document.getElementById('email'), password: document.getElementById('password') })
       .then(data => {
           window.location.replace("https://compx576.herokuapp.com/success")
       }).catch(err => {
           window.location.replace("https://compx576.herokuapp.com/fail")
       })
 
-    // fetch('https://compx576.herokuapp.com/users/login?email=' + email +'?password=' + password).then((response) => {
-    //     method: "POST"
-    //     response.json().then((data) => {
-    //         if (data.error) {
-    //             console.log(data.error)
-    //         } else {
-    //             window.location.replace("https://compx576.herokuapp.com/success");
-    //         }
-    //     })
-    // })
 })
-
 
 async function postData(url = '', data = {}) {
     // Default options are marked with *
