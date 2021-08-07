3e7a5b922f6bc5ef822eae067c05bc96949fe22b
Nick <nj78@students.waikato.ac.nz>
2021-08-08 09:22:59 +1200

    last commit here for next prototype. saving this work to a new branch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,76 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-import Assets from './assets/pages/Assets';
-import NewAsset from './assets/pages/NewAsset';
 import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import UserPlaces from './places/pages/UserPlaces';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+import { useAuth } from './shared/hooks/auth-hook';
 
 const App = () => {
-  return <Router>
-    <Switch>
-      <Route path="/" exact>
-        <Assets />
-      </Route>
-      <Route path="/assets/new" exact>
-        <NewAsset />
-      </Route>
-      <Redirect to="/" />
-    </Switch>
-  </Router>
+  const { token, login, logout, userId } = useAuth();
+
+  let routes;
+
+  if (token) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: !!token,
+        token: token,
+        userId: userId,
+        login: login,
+        logout: logout
+      }}
+    >
+      <Router>
+        <MainNavigation />
+        <main>{routes}</main>
+      </Router>
+    </AuthContext.Provider>
+  );
 };
 
 export default App;
--- frontend/src/assets/pages/NewAsset.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NewAsset = () => {
-    return <h2>NewAsset Works!</h2>
-};
-
-export default NewAsset;
