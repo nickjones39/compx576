5cf9c424131d057a7c0c01aee379c5edac86e58b
Nick <nj78@students.waikato.ac.nz>
2021-08-08 16:11:36 +1200

    bug fixing

--- models/asset.js
+++ models/asset.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const assetSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
     category: { type: mongoose.ObjectId, ref: 'Category' },
     location: { type: mongoose.ObjectId, ref: 'Location' },
     serialNumber: { type: String, unique: true },
