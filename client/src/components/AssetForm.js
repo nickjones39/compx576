61a8370e2f9596b876bb77144b162d2897887be3
Nick <nj78@students.waikato.ac.nz>
2021-08-14 11:58:09 +1200

    Working on create asset

--- client/src/components/AssetForm.js
+++ client/src/components/AssetForm.js
@@ -18,7 +18,7 @@ const AssetForm = ({
           name: assetToUpdate.name,
           category: assetToUpdate.category._id ?? '610f2f4ea7115100151ea10e',
           location: assetToUpdate.location._id ?? '610f4ed1c40f480015e66f61', // 610f4ed1c40f480015e66f61
-          assignedTo: assetToUpdate.user._id ?? '61161b109996fddae8d5696c',
+          assignedTo: assetToUpdate.user._id ?? '61048e5b7d0a89980ae44c22',
           serialNumber: assetToUpdate.serialNumber,
           model: assetToUpdate.model,
           description: assetToUpdate.description,
@@ -29,7 +29,7 @@ const AssetForm = ({
           description: '',
           category: '610f2f4ea7115100151ea10e', // hardcode a category id for assets with unassigned category
           location: '610f4ed1c40f480015e66f61', // hardcode a location id for assets with unassigned location 610f4ed1c40f480015e66f61
-          assignedTo: '61161b109996fddae8d5696c',
+          assignedTo: '61048e5b7d0a89980ae44c22',
           serialNumber: '',
           model: '',
           description: '',
@@ -122,7 +122,8 @@ const AssetForm = ({
           </Form.Control>
         </Form.Group>
 
-        
+        {/* 
+
         <Form.Group>
           <Form.Label>Assigned To</Form.Label>
           <Form.Control
@@ -139,6 +140,7 @@ const AssetForm = ({
           </Form.Control>
         </Form.Group>
 
+            */}
 
         <Form.Group>
           <Form.Label>Serial Number</Form.Label>
