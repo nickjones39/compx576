f37073ada41fe7ec69ba6e90dc5dbfc61aec2926
Nick <nj78@students.waikato.ac.nz>
2021-08-14 12:14:03 +1200

    Working on create asset

--- client/src/components/AssetForm.js
+++ client/src/components/AssetForm.js
@@ -16,8 +16,8 @@ const AssetForm = ({
     assetToUpdate
       ? {
           name: assetToUpdate.name,
-          category: assetToUpdate.category._id ?? '610f2f4ea7115100151ea10e',
-          location: assetToUpdate.location._id ?? '610f4ed1c40f480015e66f61', // 610f4ed1c40f480015e66f61
+          category: assetToUpdate.category._id ?? '6117053a9363b60016effb5e',
+          location: assetToUpdate.location._id ?? '6117050a9363b60016effb5c', // 610f4ed1c40f480015e66f61
           /* assignedTo: assetToUpdate.user._id ?? '61048e5b7d0a89980ae44c22', */
           serialNumber: assetToUpdate.serialNumber,
           model: assetToUpdate.model,
@@ -27,8 +27,8 @@ const AssetForm = ({
       : {
           name: '',
           description: '',
-          category: '610f2f4ea7115100151ea10e', // hardcode a category id for assets with unassigned category
-          location: '610f4ed1c40f480015e66f61', // hardcode a location id for assets with unassigned location 610f4ed1c40f480015e66f61
+          category: '6117053a9363b60016effb5e', // hardcode a category id for assets with unassigned category
+          location: '6117050a9363b60016effb5c', // hardcode a location id for assets with unassigned location 610f4ed1c40f480015e66f61
           /* assignedTo: '61048e5b7d0a89980ae44c22', */
           serialNumber: '',
           model: '',
