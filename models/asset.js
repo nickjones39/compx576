908307355fc57ac9429fc0a4a69d59b41e4c69bc
Nick <nj78@students.waikato.ac.nz>
2021-09-07 10:37:24 +1200

    Referenced Data Model with mongoose

--- models/asset.js
+++ models/asset.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const assetSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    category: { type: mongoose.ObjectId, ref: 'Category' },
-    location: { type: mongoose.ObjectId, ref: 'Location' }, // Location
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    location: { type: mongoose.Schema.Types.ObjectId, ref: 'Location' }, // Location
     /* Added user here */
-    // assignedTo: { type: mongoose.ObjectId, ref: 'User' },
+    assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     serialNumber: { type: String }, //  unique: true
     assetID: { type: String }, 
     model: { type: String },
