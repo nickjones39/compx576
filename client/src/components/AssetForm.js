968be89646f6de20390445b1329e1a02cb681811
Nick <nj78@students.waikato.ac.nz>
2021-09-08 10:59:13 +1200

    working on function to allow assigning assets to users

--- client/src/components/AssetForm.js
+++ client/src/components/AssetForm.js
@@ -37,7 +37,7 @@ const AssetForm = ({
 
   const onChange = (e) => {
 
-    alert(e.name);
+    alert(e.target.name);
 
     document.getElementById('editing').classList.remove('hidden');
     document.getElementById('submitted').classList.add('hidden');
