f56879888959c0c9a356b50eb6cde2f6c502c528
Nick <nj78@students.waikato.ac.nz>
2021-09-08 21:36:25 +1200

    ...... too many hours

--- client/src/components/AssetForm.js
+++ client/src/components/AssetForm.js
@@ -217,7 +217,7 @@ const AssetForm = ({
               >
                   
                   {usersList?.map((x) => (
-                    <option key={x._id} value={x._id} selected={asset.assignedTo ? asset.assignedTo == x._id ? true : 'true' : 'false'} >{x.name}</option>
+                    <option key={x._id} value={x._id} selected={asset.assignedTo ? asset.assignedTo == x._id ? true : true : false} >{x.name}</option>
                   ))}
 
                   {...disableAssignedToSelect} 
