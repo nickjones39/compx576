5d801fc0d438b32f49ac737a758457abc29d64e1
Nick <nj78@students.waikato.ac.nz>
2021-09-08 18:49:21 +1200

    working on function to allow assigning assets to users

--- client/src/components/AssetForm.js
+++ client/src/components/AssetForm.js
@@ -27,7 +27,7 @@ const AssetForm = ({
           name: '',
           category: '61171936a9e02900161fb08a', // hardcode a category id for assets with unassigned category
           location: '611718e8a9e02900161fb086', // hardcode a location id for assets with unassigned location 610f4ed1c40f480015e66f61
-          assignedTo: null,                       // default is not assigned
+          //assignedTo: null,                       // default is not assigned
           serialNumber: '',
           model: '',
           description: '',
