f4ce3da08be0c2cee049a15395f53bcfa1677ccf
Nick <nj78@students.waikato.ac.nz>
2021-09-13 16:10:50 +1200

    Starting updating the non-admin features

--- utils/define-asset-search-query.js
+++ utils/define-asset-search-query.js
@@ -1,32 +1,13 @@
-var mongoose = require('mongoose');
 
 const defineSearchQuery = (req) => {
-  
-  /*
-  try {
-    var id = mongoose.Types.ObjectId(req.query.search);
 
-    return req.query.search
-    ? {
-        $or: [
-          { name: { $regex: req.query.search, $options: 'i' } },
-          //{ description: { $regex: req.query.search, $options: 'i' } },
-          { userId: { $eq: id } },
-        ],
-      }
-    : {};
-
-  }
-  catch {
-
-  }
-*/
   return req.query.search
     ? {
         $or: [
           { "name": { $regex: req.query.search, $options: 'i' } },
           { "description": { $regex: req.query.search, $options: 'i' } },
           { "assignedTo._id": { $regex: req.query.search, $options: 'i'  } },
+          { "assignedTo.XXX": { $regex: req.query.search, $options: 'i'  } },
           { "assignedTo.userId": { $regex: req.query.search, $options: 'i'  } },
         ],
       }
