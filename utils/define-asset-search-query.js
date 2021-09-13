9d1167b264d58d806415e9f596e9f204a6f64e08
Nick <nj78@students.waikato.ac.nz>
2021-09-13 16:05:01 +1200

    Starting updating the non-admin features

--- utils/define-asset-search-query.js
+++ utils/define-asset-search-query.js
@@ -24,10 +24,10 @@ const defineSearchQuery = (req) => {
   return req.query.search
     ? {
         $or: [
-          { "data.name": { $regex: req.query.search, $options: 'i' } },
-          { "data.description": { $regex: req.query.search, $options: 'i' } },
-          { "data.assignedTo._id": { $regex: req.query.search, $options: 'i'  } },
-          { "data.assignedTo.userId": { $regex: req.query.search, $options: 'i'  } },
+          { "name": { $regex: req.query.search, $options: 'i' } },
+          { "description": { $regex: req.query.search, $options: 'i' } },
+          { "assignedTo._id": { $regex: req.query.search, $options: 'i'  } },
+          { "assignedTo.userId": { $regex: req.query.search, $options: 'i'  } },
         ],
       }
     : {};
