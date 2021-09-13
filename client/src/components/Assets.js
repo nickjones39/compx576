ea3bc58d470f8721c3d27545d6ed990db3ed6ac5
Nick <nj78@students.waikato.ac.nz>
2021-09-13 18:37:37 +1200

    Starting updating the non-admin features

--- client/src/components/Assets.js
+++ client/src/components/Assets.js
@@ -35,9 +35,9 @@ const Assets = ({ dispatch, assets, loading, error, refreshAfterError }) => {
             ) : (
               assets.map((asset, index) => {
                 
-                let myUser = document.getElementById("userHack").innerHTML;
-                console.log("User ID: " + myUser);
-                console.log("Email : " + asset.assignedTo?.email);
+                //let myUser = document.getElementById("userHack").innerHTML;
+                //console.log("User ID: " + myUser);
+                //console.log("Email : " + asset.assignedTo?.email);
 
                 //if(asset.assignedTo?.email === myUser) {
                   return (
