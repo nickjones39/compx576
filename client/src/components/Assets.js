e0dff3c80203234076ea84b72c8542982b69dd08
Nick <nj78@students.waikato.ac.nz>
2021-09-26 14:25:00 +1300

    Working on feature to allow user to request asset

--- client/src/components/Assets.js
+++ client/src/components/Assets.js
@@ -48,8 +48,8 @@ const Assets = ({ dispatch, assets, loading, admin, error, refreshAfterError }) 
                       <td>{asset.name}</td>
                       <td>{asset.category?.name}</td>
                       <td>{asset.location?.name}</td>
-                      {asset.location?.name === "In Stock" && admin ? (
-                          <td>Request Asset</td>
+                      {asset.location?.name === "In Stock" ? (
+                          <td><Button as='input' type='submit' value='Request Asset' /></td>
                         ) : (
                           <td>{asset.assignedTo?.name}</td>
                         )
